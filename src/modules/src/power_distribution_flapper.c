a16e6852ca146cfcd31a2ade3cb8baccf54d4594
SEOSUK <seosu99@seoultech.ac.kr>
2026-06-08 22:22:49 +0900

    일단 법벡추 됨

--- src/modules/src/power_distribution_flapper.c
+++ src/modules/src/power_distribution_flapper.c
@@ -14,7 +14,7 @@
  * the Free Software Foundation, in version 3.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * but WITHOUT ANY WARRㄹANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
